f5e8d85f828edc3def61a3578fa98579906052e5
Addow Abdi <adowabdi007@icloud.com>
2025-01-23 09:33:56 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -4,6 +4,26 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+server.delete('/api/users/:id', async(req, res) => {
+    const possibleUser = await User.findById(req.params.id)
+    try {
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        } else {
+            const deletedUser = await User.remove(possibleUser.id)
+            res.status(200).json(deletedUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error deleting user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
 server.post('/api/users', (req, res) => {
     const user = req.body
     if(!user.name || !user.bio) {
